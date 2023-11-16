7674f0713382d1f97c887ecf28b274bdf45f7f72
Mastermines1 <85805049+mastermines1@users.noreply.github.com>
2023-11-16 15:13:55 +0000

    added boilerplate and XML parsing

--- SSAANIP/MainWindow.xaml.cs
+++ SSAANIP/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace SSAANIP
 {
@@ -27,24 +31,39 @@ namespace SSAANIP
 
         }
 
-        private void showTheme(object sender, RoutedEventArgs e){
-            if(lbTheme.Visibility == Visibility.Visible){
-                lbTheme.Visibility = Visibility.Collapsed;
+        private void changeTheme(object sender, RoutedEventArgs e) {
+            if(this.Background == Brushes.White) {
+                this.Background = Brushes.Black; // "#1F1B24";
             }
             else{
-                lbTheme.Visibility = Visibility.Visible;
+                this.Background = Brushes.White;
             }
+        }
 
+        private void showSettings(object sender, RoutedEventArgs e)
+        {
+           if (lbSettings.Visibility == Visibility.Visible)
+            {
+                lbSettings.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                lbSettings.Visibility = Visibility.Visible;
+            }
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            getRequest index = new("100.73.164.110:4533","admin","1.16","getIndexes","test");
-            var result = await index.sendRequestAsync();
+            Request index = new("100.73.164.110:4533","admin","1.16","getArtist", "test","&id=61dbf9c1dc2a19880b44ed9fc93be334" );
+            var xmlDoc = await index.sendRequestAsync();
+            
+            foreach(XElement element in xmlDoc.Elements().Elements())
+            {
+                   
+            }
 
-            output.Content = result;
 
-            if (result.Contains($"\"ok\""))
+            /* if (result.Contains($"\"ok\""))
             {                    
 
                 char[] resultlist = result.ToCharArray();
@@ -89,7 +108,7 @@ namespace SSAANIP
                                 valueFlag = true;
                             }
                         }
-
+                        
                         if(keyFlag && valueFlag)
                         {
                             data.Add(key, value);
@@ -101,7 +120,7 @@ namespace SSAANIP
                 }
                 output.Content = data["id"]; 
 
-            }
+            } */
 
 
 
@@ -114,5 +133,47 @@ namespace SSAANIP
             //165142354302
 
         }
+
+
+
+        public async Task<IEnumerable<XElement>> System(string requestType)
+        {
+            Request request = new("100.73.164.110:4533", "admin", "1.16", requestType, "test");
+            return await request.sendRequestAsync();
+
+        }
+        public async Task<IEnumerable<XElement>> Browsing(string requestType,string id)
+        {
+            if (id != null){
+                id = $"&id={id}";
+            }
+            Request request = new("100.73.164.110:4533", "admin", "1.16", requestType, "test",id);
+            return await request.sendRequestAsync();
+        }
+        public async Task<IEnumerable<XElement>> sendCreatePlaylist(string name, string[] songId)
+        {
+            string extras = $"&name={name}";
+            foreach (string sID in songId){
+                extras += $"&songID={sID}";
+            }
+            Request request = new("100.73.164.110:4533", "admin", "1.16", "createPlaylist", "test",extras);
+            return await request.sendRequestAsync();
+        }
+        public async Task<IEnumerable<XElement>> sendUpdatePlaylist(string playlistID, string name, string comment, string isPublic, string[] songID, string[] songIndex)
+        {
+            string extras = string.Empty;
+            if (name != null){
+                extras += $"&name={name}";
+            }
+            else if (comment != null){
+                extras += $"&comment={comment}";
+            }
+
+            Request request = new("100.73.164.110:4533", "admin", "1.16", "updatePlaylist", "test", extras);
+            return await request.sendRequestAsync();
+
+        }
+
+
     }
 }
