e1c38196151190f7d77f9d108c2fe265c09b6278
Mastermines1 <85805049+mastermines1@users.noreply.github.com>
2023-11-30 15:11:42 +0000

    chnnged to pge

---
+++ SSAANIP/master.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace SSAANIP
+{
+
+    public partial class master : Window{
+
+        public master(){
+            InitializeComponent();
+
+            Frame.Content = new loginPage(this);
+        }
+    }
+}
