d77e28094526475c3954c7911b55aa90acf1a6cd
mastermines1 <milespatrickcampbell@gmail.com>
2023-12-01 10:35:05 +0000

    login page

--- SSAANIP/login.cs
+++ SSAANIP/login.cs
@@ -1,9 +1,10 @@
-﻿using System.Net.Sockets;
-using System;
+﻿using System.Net;
 using System.Windows.Controls;
 using System.Windows;
 using System.Collections.Generic;
 using System.Xml.Linq;
+using System.Linq;
+using System.Data.SQLite;
 
 namespace SSAANIP
 {
@@ -11,29 +12,37 @@ namespace SSAANIP
         public master parent;
         public RequestMethods req;
         public string socket;
-        public string username;
         public string version;
         public string appName;
+        SQLiteConnection conn;
         public loginPage(master master){
 
             InitializeComponent();
             parent = master;
-
-        }
-        public async void login(object sender, RoutedEventArgs e)
-        {
             socket = "100.73.164.110:4533";
             version = "1.16";
             appName = "test";
-            username = usrBox.Text.ToString();
-            string password = pwdBox.Text.ToString();
+            conn = new SQLiteConnection("Data source=usrDB.db");
+        }
+        public async void login(object sender, RoutedEventArgs e)
+        {
+            string username = usrBox.Text.ToString();
+            string password = pwdBox.Password.ToString();
             req = new(socket, username, version, appName);
 
             IEnumerable<XElement> response = await req.System("ping");
 
-            output.Content=response;
-
-
+            if (response.Attributes("status").First().Value.ToString() == "\"ok\"")//valid username and password
+            {
+                login(username, password);
+            }
+        }
+        public void login(string username,string password)
+        {
+           conn.Open();
+            SQLiteCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "INSERT ";
+            
         }
     }
 }
--- SSAANIP/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ SSAANIP/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace SSAANIP {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SSAANIP;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SSAANIP;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
