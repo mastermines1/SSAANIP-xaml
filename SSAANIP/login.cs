d33ee820403acd3e2274b974b8c68c9b1a5593ff
Mastermines1 <85805049+mastermines1@users.noreply.github.com>
2024-01-04 18:28:17 +0000

    updateSdr

--- SSAANIP/login.cs
+++ SSAANIP/login.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Xml.Linq;
 using System.Data.SQLite;
 using System.IO;
+using System.Linq;
 
 namespace SSAANIP{
     public partial class loginPage : Page{
@@ -24,16 +25,14 @@ namespace SSAANIP{
             Request request = new(username, password, "ping");
             var response = await request.sendRequestAsync();
             
-            foreach( XAttribute attribute in response.Attributes() ){
-                if (attribute.Name == "status" && attribute.Value.ToString() == "ok"){ //valid username and password
-
-                    parent.Frame.Content = new MainWindow(parent, username, pwdBox.Password.ToString());
-                    break;
-                }
-                else{
-                    output.Content = "Invalid username or password";
-                }
+            if (response.First().Attribute("status").Value.ToString() == "ok"){ //valid username and password
+                parent.Frame.Content = new MainWindow(parent, username, pwdBox.Password.ToString());
             }
+            else{
+                output.Content = "Invalid username or password";
+                pwdBox.Clear();
+            }
+            
         }
         public void changeServer(object sender, RoutedEventArgs e){
             File.Delete("config.txt");
