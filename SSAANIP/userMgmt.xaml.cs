bb51474d5212d8b8e548a410aed6aa0e1ec42cf4
Miles Campbell <MilesPatrickCampbell@gmail.com>
2024-01-07 19:16:59 +0000

    Moved update methods to a seperate class. Added lsQueue to display and manage the queue. Fixed bugs surrounding songIndex from the SQL db

---
+++ SSAANIP/userMgmt.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace SSAANIP{
+    /// <summary>
+    /// Interaction logic for userMgmt.xaml
+    /// </summary>
+    public partial class userMgmt : Page{
+        master master;
+        public userMgmt(master master){
+            InitializeComponent();
+            this.master = master;
+
+        }
+        
+    }
+}
