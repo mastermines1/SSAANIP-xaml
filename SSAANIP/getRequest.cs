99e03468f89b591875cc97a95024a089bd5ada63
Miles Campbell <MilesPatrickCampbell@gmail.com>
2024-01-14 19:56:33 +0000

    Worked on post requests for adding users

--- SSAANIP/getRequest.cs
+++ SSAANIP/getRequest.cs
@@ -6,6 +6,11 @@ using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Security.Cryptography;
 using System;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
 
 namespace SSAANIP{
     public class Request{
@@ -14,7 +19,6 @@ namespace SSAANIP{
 		private string version; //the version of the server
         private string request; //the type of request
         private string clientName; //the name of the client in use
-        private HttpClient client = new();
         private string extraParms;
         private string password;
         SQLiteConnection conn;
@@ -52,6 +56,7 @@ namespace SSAANIP{
         }
 
         public async Task<IEnumerable<XElement>> sendRequestAsync(){
+            HttpClient client = new();
             MD5 md5 = MD5.Create();
             string salt = createSalt(16);
             byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(password + salt);
@@ -65,7 +70,44 @@ namespace SSAANIP{
             IEnumerable<XElement> collection = XDocument.Parse(await responseMessage.Content.ReadAsStringAsync()).Elements();
             return collection;
         }
-        public static string createSalt(int size){ //generates a salt of set size 
+        public async void sendCreateUser(string newUsername, string newPassword, string IsAdmin){
+            HttpClient client = new HttpClient();
+
+            string login_url = $"http://{socket}/auth/login";
+            Dictionary<string, string> login_data = new(){
+                {"username",this.username}, {"password",this.password},
+            };
+            string json = JsonConvert.SerializeObject(login_data);
+            StringContent stringContent = new(json, System.Text.Encoding.UTF8, "application/json");
+            HttpResponseMessage login_response = await client.PostAsync(login_url, stringContent);
+            login_response.EnsureSuccessStatusCode();
+            string responseBody = await login_response.Content.ReadAsStringAsync();
+            JObject response = (JObject)JToken.Parse(responseBody);
+            var token = response.SelectToken("token").ToString();
+
+
+            if (token != null){
+                string create_user_url = $"http://100.73.164.110:4533/api/user";
+
+                Dictionary<string, string> user_data = new(){
+                    {"isAdmin", IsAdmin},
+                    {"userName", newUsername},
+                    {"name", newUsername},
+                    {"password", newPassword},
+                };
+
+                client.DefaultRequestHeaders.Add("x-nd-authorization", "Bearer " + token);
+                //json = $"{{\"isAdmin\":{IsAdmin},\"username\":\"{newUsername}\",\"name\":\"{newUsername}\",\"password\":\"{newPassword}\"}}";
+                //json = JsonConvert.SerializeObject (user);
+                var content = new FormUrlEncodedContent(user_data);
+                //stringContent = new(json, System.Text.Encoding.UTF8, "application/json");
+                HttpResponseMessage create_user_response = await client.PostAsync(create_user_url, content);
+                create_user_response.EnsureSuccessStatusCode();
+                responseBody = await create_user_response.Content.ReadAsStringAsync();
+            }
+        }
+        public static string createSalt(int size)
+        { //generates a salt of set size 
             RandomNumberGenerator rng = RandomNumberGenerator.Create();
             byte[] salt = new byte[size];
             rng.GetBytes(salt);
